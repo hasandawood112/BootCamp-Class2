ad9ab693db319f1c021693a29f9e001c2e399a78
Hasan Dawood <hasandawood555@gmail.com>
2020-06-17 01:01:49 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World, My name is Hasan Dawood.
+          Hello World My name is Hasan Dawood.
         </p>
       </header>
     </div>
